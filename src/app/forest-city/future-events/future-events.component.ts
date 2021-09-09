c69f915ad03d6c3f2fe76a2a860b12f4c99bcd5d
davidjonesfcl388 <davejones216@gmail.com>
2021-09-09 07:22:36 -0400

    initial commit for contact us

--- src/app/forest-city/future-events/future-events.component.ts
+++ src/app/forest-city/future-events/future-events.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import events from '@data/events.json';
 
 // TODO: move to common interfaces
