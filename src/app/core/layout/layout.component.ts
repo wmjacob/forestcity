405f98e05b7284ebcab00c82721593d900ea3a45
Bill <bzeligson@brightedge.com>
2021-09-17 12:20:16 -0400

    Resolve email issues hopefully
    Change-Id: I73b7675e6d42283133291e8a3a6239cf8322418d

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -8,13 +8,13 @@ import { AlertService } from '@services/alert';
   templateUrl: './layout.component.html',
   styleUrls: [ './layout.component.scss' ],
   animations: [
-    trigger('slideInOut', [
+    trigger('fadeInOut', [
       transition(':enter', [
         style({opacity: 0}),
-        animate(200, style({ opacity: 1 }))
+        animate(250, style({ opacity: 1 }))
       ]),
       transition(':leave', [
-        animate(1000, style({ opacity: 0, height: 0 }))
+        animate(250, style({ opacity: 0 }))
       ])
     ])
   ],
