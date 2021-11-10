b44e643e9e5d3a906f717d1356fd7421b06898c9
davejones216 <davejones216@gmail.com>
2021-11-09 20:38:32 -0500

    using google-spreadsheets package

--- routes/sheets-api.js
+++ routes/sheets-api.js
@@ -3,6 +3,7 @@ const sheetsRouter = express.Router();
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 const googleSheets = require('@googleapis/sheets');
 const fs = require('fs');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 const SHEETS_SECRET = 'sheets-credentials';
 const SHEETS_ID_SECRET = 'sheets-id';
@@ -30,60 +31,50 @@ sheetsRouter.post('/append-rsvp', async function(request, response) {
     try {
         const data = request.body;
         const eventNameDate = data.event.name + ' ' + data.date; // event name and date for sheets/tabs
-        // check if the sheet exists, if so, append, if not, create the page and add the row
+        // TODO check if the sheet exists, if so, append, if not, create the page and add the row
 
+        const rsvpSheet = await getAuth(SHEETS_ID_SECRET);
+        const doc = new GoogleSpreadsheet(rsvpSheet.sheetId);
         const sheetsSecret = await getAuth(SHEETS_SECRET);
 
-        // const sheetsCreds = {
-        //     client_email: sheetsSecret.client_email,
-        //     private_key: sheetsSecret.private_key
-        // }
+        await doc.useServiceAccountAuth({
+            client_email: sheetsSecret.client_email,
+            private_key: sheetsSecret.private_key
+          });
 
-        let jwtClient = new googleSheets.auth.JWT(sheetsSecret.client_email,
-                                            null,
-                                            sheetsSecret.private_key,
-                                            ["https://www.googleapis.com/auth/spreadsheets"]);
+        await doc.loadInfo();
 
-        jwtClient.authorize(function(err, tokens) {
-            if (err) {
-                console.log(err);
-                return;
-              } else {
-                console.log("Successfully connected!");
-              }
+        let sheet = doc.sheetsByTitle(eventNameDate);
+        await sheet.addRow({
+            'Last Name': data.lastName,
+            'First Name': data.firstName,
+            'Email': data.email,
+            'Early Bird Dinner': data.earlyBirdDinner,
+            'Number of Meals': data.numberOfMeals,
+            'Meal Choice': data.mealSelection
         });
 
-        // const auth = new googleSheets.auth.GoogleAuth({
-        //     credentials: sheetsCreds,
-        //     scopes: [
-        //         'https://www.googleapis.com/auth/spreadsheets'
-        //     ]
-        // });
+        await sheet.addRow(
+            [data.lastName, data.firstName, data.email, data.earlyBirdDinner, data.numberOfMeals, data.mealSelection]
+        );
 
-        // const authClient = await auth.getClient();
-        const sheets = googleSheets.sheets({version: 'v4', auth: jwtClient});
-        const rsvpSheet = await getAuth(SHEETS_ID_SECRET);
-
-        let sheetsRequest = await sheets.spreadsheets.values.get({spreadsheetId: rsvpSheet.sheetId});
-        console.log(sheetsRequest.data);
-
-        const res = await sheets.spreadsheets.values.append({
-            spreadsheetId : rsvpSheet.sheetId,
-            range : "A1:F1",
-            valueInputOption: 'USER_ENTERED',
-            requestBody: {
-              values: [
-                [data.lastName, data.firstName, data.email, data.earlyBirdDinner, data.numberOfMeals, data.mealSelection]
-              ],
-            },
-          });
-          console.log(res.data);
+        // const res = await sheets.spreadsheets.values.append({
+        //     spreadsheetId : rsvpSheet.sheetId,
+        //     range : "A1:F1",
+        //     valueInputOption: 'USER_ENTERED',
+        //     requestBody: {
+        //       values: [
+        //         [data.lastName, data.firstName, data.email, data.earlyBirdDinner, data.numberOfMeals, data.mealSelection]
+        //       ],
+        //     },
+        //   });
+        //   console.log(res.data);
     }
     catch (error) {
         console.log(error);
         response.status(500).json({error: 'Internal Service Error'});
     }
-
+    response.status(200).json({ status: 'Ok' });
 });
 
 module.exports = sheetsRouter;
