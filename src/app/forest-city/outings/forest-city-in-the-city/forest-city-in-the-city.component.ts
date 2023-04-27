938c734b35b34e389d3d4ee4c6c4ae158881a9ef
davejones216 <davejones216@gmail.com>
2023-04-26 20:53:34 -0400

    initial outline for outing page

---
+++ src/app/forest-city/outings/forest-city-in-the-city/forest-city-in-the-city.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'forest-city-in-the-city',
+    templateUrl: './forest-city-in-the-city.component.html',
+    styleUrls: [ './forest-city-in-the-city.component.scss']
+})
+export class ForestCityInTheCityComponent implements OnInit {
+
+    ngOnInit() {
+
+    }
+
+    
+}
